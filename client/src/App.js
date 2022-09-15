b79bf1fcb19a6eadb14f20011a9f3a7a7cbe9ebd
Sean Oh <ohsean812@gmail.com>
2022-09-15 09:24:35 -0400

    added search bar, avatar icon

--- client/src/App.js
+++ client/src/App.js
@@ -84,7 +84,7 @@ function App() {
         <Route exact path='/:id/:review_id/edit'>
           <EditReview />
         </Route>
-        <Route exact path='/users/:id'>
+        <Route exact path='/:id'>
           <UserProfile user={user} />
         </Route>
         <Route exact path ="/">
